0ce2edda86433e3ed5bcecfe22fbb7c654e410e3
eon <noe.navarro1019@gmail.com>
2021-12-25 20:50:11 -0800

    updated code to require response to some variables

--- app.js
+++ app.js
@@ -15,12 +15,46 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input', 
             name: 'github',
-            message: 'Provide some information about yourself:'
+            message: 'What is your github username? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if(confirmAbout){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ])
 }
@@ -42,12 +76,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the projects name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description', 
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
